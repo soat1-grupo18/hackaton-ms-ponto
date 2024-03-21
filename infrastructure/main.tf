e29013617ae57acd22f05b8ea923e8f47ad58157
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2024-03-21 19:17:58 -0300

    feat: adding infrastructure (terraform) directoty

---
+++ infrastructure/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket         = "289389227463-terraform-backend"
+    key            = "github/soat1-grupo18/hackaton-ms-ponto"
+    dynamodb_table = "289389227463-terraform-backend"
+    region         = "sa-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      terraform = "true"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
