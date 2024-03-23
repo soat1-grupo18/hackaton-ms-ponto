db713bfee3cedcd7bd174eb23007119c94bdc9ea
Pablo Lima Dias <pablo.dias@pfizer.com>
2024-03-23 10:51:42 -0300

    Initialize data (#6)

---
+++ src/main/java/br/com/fiap/soat/pontos/config/DynamoDBDataInitializer.java
@@ -0,0 +1,36 @@
+package br.com.fiap.soat.pontos.config;
+
+import br.com.fiap.soat.pontos.dynamodb.entities.PontoDynamoEntity;
+import br.com.fiap.soat.pontos.dynamodb.repositories.PontoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class DynamoDBDataInitializer {
+
+    @Autowired
+    private PontoRepository pontoRepository;
+
+    @Bean
+    public CommandLineRunner initializeData(PontoRepository pontoRepository) {
+        return args -> {
+            List<String> usernames = Arrays.asList("pablold", "mvsouza", "gisele_m", "olegon");
+            List<String> timestamps = Arrays.asList("2024-02-20T09:00:00", "2024-02-20T12:30:00", "2024-02-20T13:30:00", "2024-02-20T18:00:00");
+
+            for (String username : usernames) {
+                for (String timestamp : timestamps) {
+                    PontoDynamoEntity ponto = new PontoDynamoEntity();
+                    ponto.setUsuario(username);
+                    ponto.setData(timestamp);
+                    pontoRepository.save(ponto);
+                }
+            }
+        };
+    }
+}
